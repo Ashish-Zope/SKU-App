4f00086bc0989350f979be699103a5b1f8c3b424
Ashish <ashish.zope93@gmail.com.com>
2020-10-10 17:40:15 +0530

    first commit

---
+++ Sku.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SKU_App
+{
+  public  class Sku
+    {
+        public int id { get; set; }
+        public string SkuName { get; set; }
+    }
+}
